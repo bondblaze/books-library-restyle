9b8964b8e14ab2b8524c27f73c59c31c16b24762
Artem Bo <bondblaze@gmail.com>
2023-06-01 19:05:51 -0700

    download books with titles

--- main.py
+++ main.py
@@ -1,13 +1,44 @@
 import requests
+import os
 from pathlib import Path
+from bs4 import BeautifulSoup
+from pathvalidate import sanitize_filename
 
-Path("books").mkdir(parents=True, exist_ok=True)
+HOME_URL = "https://tululu.org"
 
-for i in range(1, 11):
-    url = f"https://tululu.org/txt.php?id={i}"
+
+def check_for_redirect(response):
+    for resp in response.history:
+        if resp.status_code != 200 or response.url == "https://tululu.org/":
+            raise requests.HTTPError
+
+
+def download_txt(url, filename, folder='books/'):
     response = requests.get(url)
     response.raise_for_status()
 
-    filename = f"books/id{i}.txt"
-    with open(filename, 'wb') as file:
+    filepath = os.path.join(folder, sanitize_filename(filename) + '.txt')
+    print(filepath)
+    with open(filepath, 'wb') as file:
         file.write(response.content)
+
+
+def main():
+    Path("books").mkdir(parents=True, exist_ok=True)
+    for i in range(1, 11):
+        url = f"https://tululu.org/b{i}/"
+        response = requests.get(url)
+        response.raise_for_status()
+        try:
+            check_for_redirect(response)
+            soup = BeautifulSoup(response.text, 'lxml')
+            book_title = soup.find('h1').text.split('::')
+            download_url = soup.find('a', string='скачать txt', href=True)
+            if download_url is not None:
+                download_txt(HOME_URL + download_url['href'], f"{i}." + book_title[0].strip(), folder='books/')
+        except requests.HTTPError:
+            continue
+
+
+if __name__ == '__main__':
+    main()
